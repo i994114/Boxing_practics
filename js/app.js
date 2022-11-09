2e5411aaa331df1805454000062b9686464e3da4
i994114 <i994114@hotmail.com>
2022-11-10 01:11:04 +0900

    ハンバーガーメニューのjs追加

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 $(function() {
-  $('.js-menuTrigger').on('click', function() {
+
+  var $btn = $('.js-menuTrigger');
+  $btn.on('click', function() {
     //ハンバーガーアイコンを×型にする処理
     $(this).toggleClass('active');
     //右からレスポンシブ用のメニューを出す処理
     $('.js-headerMenu').toggleClass('active');
+  });
+
+  //レスポンス時、SPメニュークリックでメニューを閉じる
+  var $btnMenu = $('.js-menu--link');
+  $btnMenu.on('click', function() {
+    $('.js-headerMenu').toggleClass('active');
+    $('.js-menuTrigger').toggleClass('active');
   });
 });
